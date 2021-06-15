88f8f19073afbbff930fa8f74977787864295459
Daniel Di Giovanni <dannyjdigio@gmail.com>
2021-06-15 15:39:54 -0400

    Added number format converter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NumberFormatConverterComponent } from './number-format-converter/number-format-converter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NumberFormatConverterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/number-format-converter/number-format-converter.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NumberFormatConverterComponent } from './number-format-converter.component';
+
+describe('NumberFormatConverterComponent', () => {
+  let component: NumberFormatConverterComponent;
+  let fixture: ComponentFixture<NumberFormatConverterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NumberFormatConverterComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NumberFormatConverterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
